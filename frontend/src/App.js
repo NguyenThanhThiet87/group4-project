8eb9e544ca45c624c9c57eaf63d5ebbce0932e3c
lycu1199 <duonglycu2909@gmail.com>
2025-10-22 15:40:07 +0700

    Add profile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,32 @@
+// import React from "react";
+// import UserList from "./UserList";
+// import AddUser from "./AddUser";
+
+// function App() {
+//   return (
+//     <div style={{ padding: "20px" }}>
+//       <h2>Quản lý người dùng</h2>
+//       <AddUser />
+//       <hr />
+//       <UserList />
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import React from "react";
-import UserList from "./UserList";
-import AddUser from "./AddUser";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import UserProfile from "./pages/UserProfile";
 
 function App() {
   return (
-    <div style={{ padding: "20px" }}>
-      <h2>Quản lý người dùng</h2>
-      <AddUser />
-      <hr />
-      <UserList />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<h1>Trang chủ</h1>} />
+        <Route path="/profile" element={<UserProfile />} />
+      </Routes>
+    </Router>
   );
 }
 
