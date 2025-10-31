f0297e82d60b801cff71856c7cd8da92c1ecc7f1
lycu1199 <duonglycu2909@gmail.com>
2025-10-31 19:31:44 +0700

    Commit lần 1

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -71,8 +71,9 @@ exports.register = async (req, res) => {
     }
 }
 
-exports.logout = (req, res) => {
+exports.logout = async (req, res) => {
     const token = req.headers['authorization'];
+    const { refreshToken } = req.body || {};
 
     if (!token) {
         return res.status(400).json({ message: 'Không tìm thấy token' });
@@ -80,6 +81,10 @@ exports.logout = (req, res) => {
 
     // Thêm token vào blacklist
     tokenBlacklist.add(token);
+
+    if (refreshToken) {
+        await RefreshToken.deleteOne({ refreshToken: refreshToken });
+    }
 
     res.status(200).json({ message: 'Đăng xuất thành công' });
 };
